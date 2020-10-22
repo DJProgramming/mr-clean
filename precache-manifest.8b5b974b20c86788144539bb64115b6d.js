0ecff7ee603a8ebd0e403109d7e376f7bc8a1871
DJDeezy <diegojimenez1023@yahoo.com>
2020-10-21 19:40:51 -0700

    Updates

---
+++ precache-manifest.8b5b974b20c86788144539bb64115b6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21562d0f6f871bac26b782b966a5bbec",
+    "url": "/mr-clean/index.html"
+  },
+  {
+    "revision": "4a202f228e859325961a",
+    "url": "/mr-clean/static/css/main.6fd3ade9.chunk.css"
+  },
+  {
+    "revision": "02a51477ba77556ac745",
+    "url": "/mr-clean/static/js/2.bb15a7dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mr-clean/static/js/2.bb15a7dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a202f228e859325961a",
+    "url": "/mr-clean/static/js/main.94d6f199.chunk.js"
+  },
+  {
+    "revision": "df271cac920d64d90b33",
+    "url": "/mr-clean/static/js/runtime-main.0c0333c7.js"
+  },
+  {
+    "revision": "09c6c5417faca808f251d0b506ee9f7b",
+    "url": "/mr-clean/static/media/Ford-GT-2017-sq.09c6c541.jpg"
+  }
+]);
